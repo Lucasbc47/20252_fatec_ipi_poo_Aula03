3f7047b420d98456e599fab3e774f0e7cac76864
Lucasbc47 <lucasbc47@gmail.com>
2025-09-24 20:37:38 -0300

    wip

--- ap008_vetor_dinamico/src/App.java
+++ ap008_vetor_dinamico/src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        VetorDinamico vetor = new VetorDinamico(2);
+        vetor.acrescentar(10);
+        vetor.acrescentar(20);
+        vetor.mostrar();
+        vetor.acrescentar(20);
+        vetor.mostrar();
+        int numero = vetor.obter(0);
+        System.out.printf("numero: %d\n", numero);
     }
 }
--- ap008_vetor_dinamico/src/VetorDinamico.java
+++ ap008_vetor_dinamico/src/VetorDinamico.java
@@ -3,21 +3,56 @@ public class VetorDinamico {
     private int[] elementos;
     private int quantidade;
 
-    public VetorDinamico(int[] elementos, int quantidade){
-        this.capacidade = 0;
-        this.elementos = elementos;
-        this.quantidade = quantidade; 
+    public VetorDinamico(int capacidadeInicial) {
+        this.capacidade = capacidadeInicial;
+        this.elementos = new int[capacidadeInicial];
+        this.quantidade = 0;
+    }
+
+    // Construtor Padrão: caso nenhuma capacidade foi passada inicialmente;
+    public VetorDinamico() {
+        this(0);
     }
 
     public boolean isCheio() {
         return this.capacidade == this.quantidade;
     }
 
-    // if is cheio cap x 2
-    public void acrescentar(){
-        if (isCheio()) {
-            this.capacidade *= 2;
+    private void redimensionar() {
+        this.capacidade = (this.capacidade == 0) ? 1 : this.capacidade * 2;
+        int[] novoArray = new int[this.capacidade];
+        for (int i = 0; i < this.quantidade; i++) {
+            novoArray[i] = this.elementos[i];
         }
+        this.elementos = novoArray;
     }
 
-}   
+    public void acrescentar(int elemento) {
+        if (isCheio()) {
+            redimensionar();
+        }
+        this.elementos[this.quantidade] = elemento;
+        this.quantidade++;
+    }
+
+    public int tamanho() {
+        return this.quantidade;
+    }
+
+    public int obter(int indice) {
+        if (indice >= 0 && indice < quantidade) {
+            return elementos[indice];
+        }
+        throw new IndexOutOfBoundsException("Índice inválido: " + indice);
+    }
+
+    public void mostrar() {
+        System.out.print("[");
+        for (int i = 0; i < quantidade; i++) {
+            System.out.print(elementos[i]);
+            if (i < quantidade - 1)
+                System.out.print(", ");
+        }
+        System.out.println("]");
+    }
+}
