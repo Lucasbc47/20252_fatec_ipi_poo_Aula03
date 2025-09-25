f498882b9f0f699f7ff53529cfdc20758b2ed38e
Lucasbc47 <lucasbc47@gmail.com>
2025-09-24 23:33:36 -0300

    test: default params

--- ap008_vetor_dinamico/src/App.java
+++ ap008_vetor_dinamico/src/App.java
@@ -1,10 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-
-        VetorDinamico vetor = new VetorDinamico(2);
+        VetorDinamico vetor = new VetorDinamico();
         vetor.acrescentar(10);
         vetor.acrescentar(20);
-        vetor.mostrar();
         vetor.acrescentar(20);
         vetor.mostrar();
         int numero = vetor.obter(0);
